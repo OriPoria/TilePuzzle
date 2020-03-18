63748d83b2ba59f5b4a8aa5c1f0a3dc5389ca308
ori poria <ori.poria@live.biu.ac.il>
2020-03-18 13:43:18 +0200

    first commit

---
+++ controls/TilePuzzle.xaml.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TilePuzzle.controls
+{
+    /// <summary>
+    /// Interaction logic for TilePuzzle.xaml
+    /// </summary>
+    public partial class TilePuzzle : UserControl
+    {
+        Label[] tiles;
+        public TilePuzzle()
+        {
+            InitializeComponent();
+            tiles = new Label[9];
+            tiles[0] = l1;
+            tiles[1] = l2;
+            tiles[2] = l3;
+            tiles[3] = l4;
+            tiles[4] = l5;
+            tiles[5] = l6;
+            tiles[6] = l7;
+            tiles[7] = l8;
+            tiles[8] = l9;
+        }
+        public string Order
+        {
+            get
+            {
+                string s = "";
+                foreach (Label l in tiles)
+                    s += l.Content.ToString();
+                return s;
+            }
+            set
+            {
+                string s = value;
+                for (int i = 0; i < 9; i++)
+                {
+                    tiles[i].Content = s[i];
+                    if (s[i] == ' ')
+                        tiles[i].Background = Brushes.Gray;
+                    else
+                        tiles[i].Background = Brushes.White;
+                }
+            }
+        }
+        public void move(int i)
+        {
+            string s = Order;
+
+            int clickedRow = i / 3;
+            int clickedColumn = i % 3;
+
+            int grayRow = 0;
+            int grayColumn = 0;
+
+            int j = 0;
+            for (j = 0; j < 9; j++)
+            {
+                if (s[j] == ' ')
+                {
+                    grayRow = j / 3;
+                    grayColumn = j % 3;
+                    break;
+                }
+            }
+            if ((grayRow == clickedRow && (grayColumn + clickedColumn) % 2 == 1) || (grayColumn == clickedColumn && (grayRow + clickedRow) % 2 == 1))
+                Order = switchChars(i, j, s);
+        }
+        public string switchChars(int i, int j, string s)
+        {
+            string newStr = "";
+
+            char a = s[i];
+            char b = s[j];
+
+            var builder = new StringBuilder();
+
+            for (int k = 0; k < 9; k++)
+            {
+                if (k == i)
+                    builder.Append(b);
+                else if (k == j)
+                    builder.Append(a);
+                else
+                    builder.Append(s[k]);
+            }
+            newStr = builder.ToString();
+            return newStr;
+        }
+        private void l1_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            move(0);
+        }
+        private void l2_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            move(1);
+        }
+        private void l3_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            move(2);
+        }
+        private void l4_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            move(3);
+        }
+        private void l5_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            move(4);
+        }
+        private void l6_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            move(5);
+        }
+        private void l7_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            move(6);
+        }
+        private void l8_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            move(7);
+        }
+        private void l9_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            move(8);
+        }
+    }
+}
